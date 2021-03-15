a0e8560b2e7811197b19db96912aaa15b6c2490f
vaishnav-197 <vaishnavea18@gmail.com>
2021-03-15 22:56:51 +0530

    boilerplate update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const chalk = require('chalk');
-
+const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-
+var path = require('path');
 const routes = require('./routes/route')
 
 
@@ -13,13 +13,23 @@ const routes = require('./routes/route')
 
 const app = express();
 
-
-
+app.use(express.static(__dirname + '/public'));
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+
+
+
+
+
+
 app.use(
     morgan(
      ':method :url :status :response-time ms'
--- routes/route.js
+++ routes/route.js
@@ -1,13 +1,11 @@
 const express = require('express')
-
-
-
 const router = express.Router()
 // Add routes here
 
 
+router.get('/', (req, res) => res.render('welcome'));
 
-
+router.get('/donate', (req, res) => res.render('donate'));
 
 
 
