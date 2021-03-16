b8ef3524501415ad1e07ea9cf73972e31c0aa831
vaishnav-197 <vaishnavea18@gmail.com>
2021-03-16 20:57:44 +0530

    login page added

--- routes/route.js
+++ routes/route.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => res.render('welcome'));
 
 router.get('/donate', (req, res) => res.render('donate'));
 
+router.get('/login', (req, res) => res.render('login'));
 
 
 module.exports = router
